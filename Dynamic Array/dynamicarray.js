cc1645c7cef715a6fce0ba9a4fb937c7c05502db
Gervig <gervig91@gmail.com>
2025-11-01 17:32:38 +0100

    Update dynamicarray.js

--- Dynamic Array/dynamicarray.js
+++ Dynamic Array/dynamicarray.js
@@ -5,8 +5,8 @@ export class DynamicArray {
   #size;
   #capacity;
 
-  constructor(capacity) {
-    this.#size;
+  constructor(capacity = 10) {
+    this.#size = 0;
     this.#capacity = capacity;
     this.#arr = new StaticArray(capacity);
   }
@@ -21,19 +21,78 @@ export class DynamicArray {
     return this.#size;
   }
 
-  get(index){}
+  add(item) {
+    // if the array has hit capacity, it's time to grow
+    if (this.#size >= this.#capacity) this.#grow();
 
-  insert(index, value){}
+    this.#arr.set(this.#size, item);
+    this.#size++;
+  }
 
-  remove(index){}
+  get(index) {
+    this.#rangeCheck(index);
+    return this.#arr.get(index);
+  }
 
-  set(index, value){}
+  insert(index, value) {
+    this.#rangeCheck(index);
+    if (index < 0 || index > this.#size) this.#grow();
 
-  clear(){}
+    for (let i = this.#size; i > index; i--) {
+      this.#arr.set(i, this.#arr.get(i - 1));
+    }
+
+    this.#arr.set(index, value);
+    this.#size++;
+  }
+
+  remove(index) {
+    this.#rangeCheck();
+
+    for (let i = index; i < this.#size; i++) {
+      this.#arr.set(i, this.#arr.get(i + 1));
+    }
+
+    this.#arr.set(this.#size - 1, null);
+    this.#size--;
+  }
+
+  set(index, value) {
+    this.#rangeCheck(index);
+    return this.#arr.set(index, value);
+  }
+
+  size() {
+    return this.#size;
+  }
+
+  capacity() {
+    return this.#capacity;
+  }
+
+  clear() {
+    this.#arr = new StaticArray(this.#capacity);
+    this.#size = 0;
+  }
 
   // this should private, you shouldn't be able to call this from the outside
-  #grow(){}
+  #grow() {
+    const newCapacity = this.#capacity * 2;
+    const newArr = new StaticArray(newCapacity);
+
+    for (let i = 0; i < this.#size; i++) {
+      newArr.set(i, this.#arr.get(i));
+    }
+    this.#arr = newArr;
+    console.log(`Array has grown from ${this.#capacity} to ${newCapacity}`);
+    this.#capacity = newCapacity;
+  }
 
   // same for this, should be private
-  #shrinking(){}
+  #shrinking() {}
+
+  #rangeCheck(index) {
+    if (index < 0 || index >= this.#size)
+      throw new RangeError(`Index ${index} is out range.`);
+  }
 }
