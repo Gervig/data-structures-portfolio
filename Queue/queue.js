a3621d0c18719a65c045ee5f820718b1f774a8ab
Gervig <gervig91@gmail.com>
2025-11-20 00:51:12 +0100

    Update queue.js

--- Queue/queue.js
+++ Queue/queue.js
@@ -15,9 +15,6 @@ export default class Queue {
   }
 
   enqueue(data) {
-    // this.addLast(data);
-    // return this.tail();
-
     const newNode = this._createNode(data);
 
     // if the list doesn't have a tail, sets new node as tail and head
@@ -41,13 +38,58 @@ export default class Queue {
     if (index < 0 || index >= this._size) {
       throw RangeError(`Index ${index} out of range`);
     }
+
+    // declare a node for removal
+    let removedNode;
+    // if index is the head
+    if (index === 0) {
+      removedNode = this.head;
+      // sets the head's node .next as the new head
+      // now the old head has no next,
+      // it's not linked and therefore 'removed' from the list
+      this.head = this.head.next;
+      // the new head should no longer have a prev
+      if (this.head) this.head.prev = null;
+      // if the list only has one node,
+      // the tail variable should also be set to null after deletion
+      if (this._size === 1) this.tail = null;
+    }
+    // if index is the tail
+    else if (index === this._size - 1) {
+      removedNode = this.tail;
+      // sets the tail's node .prev as the new tail
+      this.tail = this.tail.prev;
+      // now the old tail has no prev,
+      // the new tail should no longer have a next
+      this.tail.next = null;
+    } else {
+      // finds the node before the 'deletion'
+      const prev = this.getNode(index - 1);
+      // finds the node after the 'deletion'
+      const next = prev.next.next;
+      // sets removal node as the prev's next
+      removedNode = prev.next;
+      // sets the prev's next as the removed node's next
+      prev.next = removedNode.next;
+      // the removed node still has a next,
+      // but no other node has a next that points to it
+      // we can therefor consider it 'removed'
+
+      // sets the node that is after the deletion's prev
+      // to be the deleted node's prev
+      next.prev = prev;
+    }
+    // size of list goes down
+    this._size--;
+
+    // return removedNode
+    return removedNode;
   }
 
   dequeue() {
-    // const node = this.head();
-    // this.list.removeFirst();
-    // return node;
-
+    if (!this.get(0)) {
+      throw RangeError(`Nothing to dequeue`);
+    }
     return this.remove(0);
   }
 
@@ -104,5 +146,17 @@ export default class Queue {
     }
 
     return current;
+  }
+
+  isEmpty() {
+    return this._size === 0;
+  }
+
+  toArray() {
+    const arr = [];
+    while (!this.isEmpty()) {
+      arr.push(this.dequeue().data);
+    }
+    return arr;
   }
 }
