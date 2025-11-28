582f7f5afe02850a18c8c875b9027c0020d995a5
Gervig <gervig91@gmail.com>
2025-11-28 21:06:36 +0100

    Update tree.js

--- Tree/tree.js
+++ Tree/tree.js
@@ -3,260 +3,17 @@ export default class Tree {
     this.root;
   }
 
-  //TODO: rewrite to non BST
   printTree() {
-    // Print the tree in a nice way - by creating a (jagged) 2D array of the tree
-    // each level (starting from root) is an array in the array that doubles in size from the previous level
-
-    // breaks if the tree is too deep - but that's a problem for another day
-
-    // Use DFS to fill array with values
-    const treeArray = [];
-    let height = 0; // and while we're at it, calculate the height of the tree
-    buildTreeArray(this.root, 0, 0);
-
-    // Does a Depth-First-Scan of the Tree,
-    // keeping track of the current depth (how far down from the top)
-    // and the current indent (how far right from the (possible) left-most node at this depth)
-    // stores the node values in a 2D array
-    function buildTreeArray(node, depth, indent) {
-      if (!node) {
-        return;
-      }
-      height = Math.max(height, depth);
-      // insert this node value in the 2D array
-      if (!treeArray[depth]) treeArray[depth] = [];
-      treeArray[depth][indent] = node.item;
-      // visit its children - remember to double indent
-      buildTreeArray(node.left, depth + 1, indent * 2);
-      buildTreeArray(node.right, depth + 1, indent * 2 + 1);
-    }
-
-    // Apparently I'm not smart enough to calculate these, so here's a pre-calculated list
-    const indentations = [1, 2, 5, 11, 23, 46, 93];
-
-    let treeString = " ";
-    // Display array - one level at a time
-    for (let depth = 0; depth < treeArray.length; depth++) {
-      const values = treeArray[depth];
-
-      // Calculate indent for this depth (or find it in the pre-calculated table)
-      let currentHeight = height - depth; // currentHeight is the distance from the bottom of the tree
-      let indent = indentations[currentHeight];
-
-      // Only display tree structure if we are not at the top
-      if (depth > 0) {
-        // Loop through half the values - and show a subtree with left and right
-        for (let i = 0; i < values.length / 2; i++) {
-          treeString += " ".repeat(indent);
-          // Only show sub-tree if there are some values below
-          if (values[i * 2] != undefined || values[i * 2 + 1] != undefined) {
-            treeString += "┌";
-            treeString += "─".repeat(indent > 1 ? indent : 0);
-            treeString += "┴";
-            treeString += "─".repeat(indent > 1 ? indent : 0);
-            treeString += "┐";
-          } else {
-            treeString += "   " + "  ".repeat(indent > 1 ? indent : 0);
-          }
-          treeString += " ".repeat(indent);
-          // add a single space before the next "block"
-          treeString += " ";
-        }
-        // and finalize the current line
-        treeString += "\n";
-      }
-
-      // Indent numbers one less than their "tree drawings"
-      // Unless it is the first one, then it is two (or maybe three) less ... mystic math!
-      if (depth == 0) {
-        treeString += " ".repeat(indent - 2);
-      } else {
-        treeString += " ".repeat(indent - 1);
-      }
-
-      // display values
-      for (let i = 0; i < values.length; i++) {
-        // if both children are undefined, don't show any of then
-        // if only one child is, show it as underscores _
-        const showUndefined =
-          !values[i - (i % 2)] && !values[i - (i % 2) + 1] ? " " : "_";
-        // if depth is lowest (height-1) - pad values to two characters
-        if (depth == height) {
-          treeString += String(values[i] ?? showUndefined.repeat(2)).padStart(
-            2,
-            " "
-          );
-          // and add a single space
-          treeString += " ";
-        } else {
-          // otherwise center values in block of three
-          treeString += String(values[i] ?? showUndefined.repeat(3))
-            .padEnd(2, " ")
-            .padStart(3, " ");
-
-          // and add twice the indentation of spaces + 1 in the middle
-          treeString += " ".repeat(indent - 1);
-          treeString += " ";
-          treeString += " ".repeat(indent - 1);
-        }
-      }
-
-      // finalize the value-line
-      treeString += "\n";
-    }
-
-    console.log(treeString);
+    
   }
 
-  //TODO: rewrite to non BST
   addValue(value) {
-    let node = this.root;
-    while (node != null) {
-      // go left
-      if (value < node.item) {
-        // if left is null (a leaf)
-        if (node.left == null) {
-          // create a new node with a parent
-          const newNode = this.createChild(value, node);
-          // maintain
-          this.maintain(node);
-          // sets the child on the parent
-          node.left = newNode;
-          break;
-        }
-        // if the left node is not null we move on
-        else {
-          node = node.left;
-        }
-      }
-      // go right
-      else if (value > node.item) {
-        // if right is null (a leaf)
-        if (node.right == null) {
-          // create a new node with a parent
-          const newNode = this.createChild(value, node);
-          // maintain
-          this.maintain(node);
-          // sets the child on the parent
-          node.right = newNode;
-          break;
-        }
-        // if the right node is not null we move on
-        else {
-          node = node.right;
-        }
-      }
-      // otherwise the value is identical
-      else {
-        return;
-      }
-    }
   }
 
-  //TODO: rewrite to non BST
   findValue(value) {
-    if (!this.root) {
-      return;
-    } else if (this.root.value == value) {
-      return this.root;
-    }
-
-    let node = this.root;
-
-    // loop through the tree
-    while (node != null) {
-      // we go right
-      if (node.left.value > value) {
-        // assign new node
-        node = node.left;
-        if (node.value == value) {
-          return node;
-        }
-      }
-      // we go left
-      else {
-        // assign new node
-        node = node.right;
-        if (node.value == value) {
-          return node;
-        }
-      }
-    }
-    // if we didn't find it, return null
-    return null;
   }
 
-  //TODO: rewrite to non BST
   removeValue(value) {
-    // find the node to remove
-    const node = this.findValue(value);
-    if (!node) return null;
-
-    const parent = node.parent;
-    const leftNode = node.left;
-    const rightNode = node.right;
-
-    // -------------------------------------------------------------------
-    // CASE 1: Node has no children
-    // -------------------------------------------------------------------
-    if (!leftNode && !rightNode) {
-      if (!parent) {
-        // node is the root
-        this.root = null;
-      } else if (parent.left === node) {
-        parent.left = null;
-      } else {
-        parent.right = null;
-      }
-      return;
-    }
-
-    // -------------------------------------------------------------------
-    // CASE 2: Node has exactly one child (left OR right)
-    // -------------------------------------------------------------------
-    if (!leftNode || !rightNode) {
-      const child = leftNode || rightNode; // whichever exists
-
-      if (!parent) {
-        // node is root
-        this.root = child;
-        child.parent = null;
-      } else if (parent.left === node) {
-        parent.left = child;
-        child.parent = parent;
-      } else {
-        parent.right = child;
-        child.parent = parent;
-      }
-
-      return;
-    }
-
-    // -------------------------------------------------------------------
-    // CASE 3: Node has two children — we use inorder successor
-    // successor = smallest node in right subtree
-    // -------------------------------------------------------------------
-    let successor = rightNode;
-    while (successor.left) {
-      successor = successor.left;
-    }
-
-    // Copy successor's value into the current node
-    node.value = successor.value;
-
-    // Delete the successor node (it has 0 or 1 child)
-    // This is safe because successor cannot have a left child
-    const succParent = successor.parent;
-    const succChild = successor.right; // successor only can have a right child
-
-    if (succParent.left === successor) {
-      succParent.left = succChild;
-    } else {
-      succParent.right = succChild;
-    }
-
-    if (succChild) succChild.parent = succParent;
   }
 
   // --------------------- //
@@ -283,11 +40,18 @@ export default class Tree {
       // This is not good enough, what if the child nodes are null?
       return this.childNodes.length == 0;
     }
+
     appendChild(child) {
       this.childNodes.push(child);
     }
+
     removeChild(child){
         //TODO: use filtering
     }
+
+    replaceChild(newChild, odlChild){
+
+    }
+
   };
 }
